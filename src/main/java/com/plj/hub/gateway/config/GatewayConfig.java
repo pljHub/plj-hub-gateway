92552bd9831f7d507d17ff49fc3602b11236bcf7
shoon95 <tkdgns5817@gmail.com>
2024-09-11 10:51:53 +0900

    feat: HUB-SERVICE 라우팅 경로 추가

--- src/main/java/com/plj/hub/gateway/config/GatewayConfig.java
+++ src/main/java/com/plj/hub/gateway/config/GatewayConfig.java
@@ -10,8 +10,21 @@ public class GatewayConfig {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("user_service", r -> r.path(PathConfig.USER_PATH)
+                .route("user_service", r -> r.path(PathConfig.USER_SERVICE)
                         .uri("lb://USER-SERVICE"))
+
+                .route("hub_service", r -> r.path(PathConfig.HUB_SERVICE)
+                        .uri("lb://HUB-SERVICE"))
+
+                .route("product_service", r -> r.path(PathConfig.PRODUCT_SERVICE)
+                        .uri("lb://HUB-SERVICE"))
+
+                .route("company_service", r -> r.path(PathConfig.COMPANY_SERVICE)
+                        .uri("lb://HUB-SERVICE"))
+
+                .route("hub_path_service", r -> r.path(PathConfig.HUB_PATH_SERVICE)
+                        .uri("lb://HUB-SERVICE"))
+
                 .build();
     }
 }
--- src/main/java/com/plj/hub/gateway/config/PathConfig.java
+++ src/main/java/com/plj/hub/gateway/config/PathConfig.java
@@ -1,6 +1,10 @@
 package com.plj.hub.gateway.config;
 
 public class PathConfig {
-    public static final String USER_PATH = "/api/users/**";
+    public static final String USER_SERVICE = "/api/users/**";
+    public static final String HUB_SERVICE = "/api/hubs/**";
+    public static final String PRODUCT_SERVICE = "/api/products/**";
+    public static final String COMPANY_SERVICE = "/api/companies/**";
+    public static final String HUB_PATH_SERVICE = "/api/hub-path/**";
 
 }
